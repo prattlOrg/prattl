9fecb61f9871af6ce68ff63e422b94a7443c52f7
voidKandy <voidkandy@gmail.com>
2024-03-05 13:17:27 -0800

    modules work!

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module prattle 
 
 go 1.20
--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"prattle/endpoint"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
 	})
+
+	fmt.Println(endpoint.Hey())
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
