5e55afd593d51234c03ac297ebc2d0d7f804f038
Ben Marshall <benleem98@yahoo.com>
2024-04-05 21:39:49 -0400

    text options are done through custom css field in obs, as browser interaction is not ideal, so much deleted, what a waste of time :'(

--- public/scripts/localStorage.js
+++
@@ -1,8 +0,0 @@
-export const storeTextOptions = (options) => {
-	window.localStorage.setItem("textOptions", JSON.stringify(options));
-};
-
-export const getTextOptions = () => {
-	const options = window.localStorage.getItem("textOptions");
-	return JSON.parse(options);
-};
