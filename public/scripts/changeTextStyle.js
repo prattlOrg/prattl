584071dafaabfff8b71c07a249c7b8523467b88d
Ben Marshall <benleem98@yahoo.com>
2024-04-05 20:08:12 -0400

    basic options form functionality, form prefill functionality

--- public/scripts/changeTextStyle.js
+++ public/scripts/changeTextStyle.js
@@ -3,9 +3,9 @@ import { getTextOptions } from "./localStorage.js";
 const changeTextStyle = (textId) => {
 	const transcriptionText = document.getElementById(textId);
 	const styles = getTextOptions();
-	transcriptionText.style.color = styles.textColor;
-	transcriptionText.style.fontSize = styles.textSize;
 	transcriptionText.style.fontFamily = styles.font;
+	transcriptionText.style.color = styles.textColor;
+	transcriptionText.style.fontSize = `${styles.textSize}px`;
 };
 
 export default changeTextStyle;
