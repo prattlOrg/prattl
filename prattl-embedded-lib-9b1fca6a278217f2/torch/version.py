a98a56b79e2b6486b913f1a7fb7e5fe1a0337a38
Ben Marshall <benleem98@yahoo.com>
2024-06-14 20:39:17 -0400

    removing files build files

--- prattl-embedded-lib-9b1fca6a278217f2/torch/version.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-__all__ = ['__version__', 'debug', 'cuda', 'git_version', 'hip']
-__version__ = '2.5.0.dev20240614'
-debug = False
-cuda: Optional[str] = None
-git_version = '3d45df9a517293a0a803df399bc10d062dc612f6'
-hip: Optional[str] = None
